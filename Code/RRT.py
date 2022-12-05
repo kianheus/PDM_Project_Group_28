e0cff9c96893bd84a0777d36ca7e24ee73a2ab9d
paugascl <paugascl.upv@gmail.com>
2022-12-05 16:21:48 +0100

    Add method to plot workspace with start and end points

--- Code/RRT.py
+++ Code/RRT.py
@@ -1,6 +1,7 @@
 import numpy as np
 import math
 import pygame
+from sys import exit
 
 class RRTCalc(self, start_x, start_y):
 
@@ -29,30 +30,56 @@ class RRTCalc(self, start_x, start_y):
 
 
 
-class RRT_plot():
+class RRTPlot():
 
-    def __init__(self, start_x, start_y, goal_x, goal_y, workspace_x, workspace_y, obstacles)
-        self.start_x = start_x
-        self.start_y = start_y
+    def __init__(self, start_x, start_y, goal_x, goal_y, workspace_x, workspace_y):
+        self.start_x = int(start_x)
+        self.start_y = int(start_y)
         
         self.goal_x = goal_x
         self.goal_y = goal_y
         self.workspace_x = workspace_x
         self.workspace_y = workspace_y
-    
-    def DrawMap():
-        pass
+        
+        self.workspace = pygame.display.set_mode(size=(self.workspace_x, self.workspace_y))   
+        self.workspace.fill((255, 255, 255))
+        self.nodeRad = 0
+        self.nodeThickness = 0
+        self.edgeThickness = 1
+        
+        self.green = (0, 255, 0)
+        self.red = (255, 0, 0)
+        self.white = (255, 255, 255)
+        self.black = (0, 0, 0)
+                                          
+    def drawWorkspace(self):
+        pygame.draw.circle(self.workspace, self.black, center = (self.start_x, self.start_y), radius = self.nodeRad+5.0)
+        pygame.draw.circle(self.workspace, self.black, center = (self.goal_x, self.goal_y), radius = self.nodeRad+5.0)
+        
     
 
-workspace_x = 20
-workspace_y = 10
+workspace_x = 600
+workspace_y = 1000
 
-start_x = 2
-start_y = 2
+start_x = 50
+start_y = 50
 start_theta = 0
 
-goal_x = 18
-goal_y = 8
+goal_x = 500
+goal_y = 500
 
+pygame.init()
+workspace = RRTPlot(start_x, start_y, goal_x, goal_y, workspace_x, workspace_y)
+workspace.drawWorkspace()
+pygame.display.update()
+pygame.event.clear()
+pygame.event.wait(0)
+
+while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+                
 
 
