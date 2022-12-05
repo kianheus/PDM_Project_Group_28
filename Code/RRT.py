89c4467a70f433fdb7a42f26ddeca88a67090243
KianHeusAD <Kianheus@gmail.com>
2022-12-05 15:27:22 +0100

    Update RRT with basic RRTCalc definition

--- Code/RRT.py
+++ Code/RRT.py
@@ -1,12 +1,50 @@
 print("Hello World!")
 
-class RRT_calc():
+import numpy as np
 
-    def method():
+
+class RRTCalc(self, start_x, start_y):
+
+
+    self.graph.x = [start_x]
+    self.graph.y = [start_y]
+    self.graph.parent = [0]
+
+
+
+    def Method():
         pass
+
+    def NewPoint():
+        new_x = np.random.uniform(0, workspace_x)
+        new_y = np.random.uniform(0, workspace_y)
+        valid_point = PathCheck(new_x, new_y)
+        pass
+
+    def PathCheck(new_x, new_y):
+        for i, (x, y) in enumerate(zip(self.graph.x, self.graph.y)):
+            print(i, x, y, new_x, new_y)
+        return True
+
+
+
 
 
 class RRT_plot():
 
     def method():
         pass
+
+
+workspace_x = 20
+workspace_y = 10
+
+start_x = 2
+start_y = 2
+start_theta = 0
+
+goal_x = 18
+goal_y = 8
+
+
+
