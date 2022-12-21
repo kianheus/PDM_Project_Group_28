c488fd74f4265e2d4964ac8e7b2c9e6d32aefb84
ultrarobotics <ultraroboticsproject@gmail.com>
2022-12-21 11:35:22 +0100

    Added longitudal pid

--- mujoco/carsim.py
+++ mujoco/carsim.py
@@ -2,6 +2,11 @@ import carenv
 import numpy as np
 import time
 
+#function used to bound output of controllers
+def bound(low, high, value):
+     return max(low, min(high, value))
+
+#function for PID controller
 class PIDcontroller():
     
     def __init__(self, kp, ki, kd):
@@ -17,66 +22,58 @@ class PIDcontroller():
         self.se = self.se + e*self.dt # intergrated error
          
         P = self.kp*e 
-        I = self.ki*self.se #Forward euler
-        D = self.kd*(e-self.pe)/dt #Forward difference 
+        I = self.ki*self.se # forward euler
+        D = self.kd*(e-self.pe)/self.dt # forward difference 
         
-        output = P + I + D # Output of pid controller
+        output = P + I + D # output of pid controller
         
         self.pe = e # previous error used in differentiation
         
         return output
 
+# initialise pid controllers
 theta_pid = PIDcontroller(1, 0, 0)
-longitudal_pid = PIDcontroller(1, 0, 0)
-        
+longitudal_pid = PIDcontroller(1, 0, 0.1)
+
+# initialise environment
 env = carenv.Car() #load in Car environment
 
 state, obstacles = env.reset() #start with reset
 
 starttime = time.time()
 
-pe = 0
-se = 0
-Kp = 1
-Ki = 0
-Kd = 0
-dt = 0.01
 thetar = 0*2*np.pi
-pr = np.array([0,0])
+pr = np.array([5,0])
 
 
 #simulate for 100 s
 while True:
     
-    #action: first numer [-0.38, 0.38] - = right, + = left. Second number [unconstrained] - backward. + = forwar
-    
-    ########################## angle    
+    ########################## angle ##########################    
     theta = state['theta']
     theta_error = ((thetar-theta+np.pi) % (2*np.pi)) - np.pi # error
     steering_angle = theta_pid.pid(theta_error)
     
-    ########################## lateral error
-    p = np.array([state['x'], state['y']])
-    
-    distance = np.linalg.norm(pr-p)
-    
-    angle = np.arctan2(p[1],p[0])
-    
+    ########################## longitudal error ##########################
+    p = np.array([state['x'], state['y']]) 
+    distance = np.linalg.norm(pr - p)  
+    angle = np.arctan2(p[1]-pr[1],p[0]-pr[0])
     difference_angle = thetar - angle
     
     lateral_error = -np.sin(difference_angle)*distance
     longitudal_error = np.cos(difference_angle)*distance
-     
-    throttle = longitudal_pid.pid(longitudal_error)
     
-    action = np.array([steering_angle,throttle]) 
+    throttle = longitudal_pid.pid(longitudal_error)    
+    throttle = bound(-1, 1, throttle)
+    
+    ########################## sim ##########################
+    action = np.array([steering_angle,-throttle])  #action: first numer [-0.38, 0.38] - = right, + = left. Second number [unconstrained] - backward. + = forwar
     state, obstacles = env.step(action) #set step in environment
     env.render(mode = True) # turn rendering on or off
 
-    #reset every 5 seconds, this can be changed    
+    ########################## reset every 20 seconds, this can be changed ##########################   
     if env.get_time() > 20:
         env.reset()
     
-    #print(state)        
-    time.sleep(0.01 - ((time.time() - starttime) % 0.01))
+    time.sleep(0.01 - ((time.time() - starttime) % 0.01)) # sleep for 100 Hz realtime loop
         
